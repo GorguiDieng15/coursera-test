efa52092049b3598fad1111f75cdf6553b70f666
GorguiDieng15 <63424579+GorguiDieng15@users.noreply.github.com>
2020-07-30 22:50:49 -0700

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -82,7 +82,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl === "categories", // ***** <---- TODO: STEP 1: Substitute [...] ******
+  allCategoriesUrl,
+  function (categories) {
+    document.querySelector("#main-content")
+      .innerHTML = categories;
+  } // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
